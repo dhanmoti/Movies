c5a5dfc0f9ce2d3d8cdc081a630ab7c3a5ec5a56
devidhan <dhanmoti@icloud.com>
2021-08-03 20:13:13 +0800

    create search ui

---
+++ MoviesApp/MoviesApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SearchViewController.swift
+//  MoviesApp
+//
+//  Created by Dhan Moti on 3/8/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movie-cell", for: indexPath)
+        
+        return cell
+    }
+}
